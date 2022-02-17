381b7fd0efdbe8e674b2325abceb9af7730d7bc6
ikasiromii28 <camelliakamesan716@gmail.com>
2022-02-17 16:19:03 +0900

    コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
   root to: 'posts#index'
+  resources :posts do
+    resources :comments, only: :create
+  end
   resources :users, only: [:show]
   resources :relationships, only: [:create, :destroy]
 end
